221586669b82b4e36143478559d9840298404bab
Michael Fleming <mfleming7024@gmail.com>
2014-06-24 21:13:00 -0400

    Added new Flat UI package and removed head tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var canvas = $("canvas");
 var currentColor = 000000;
 var ctx = document.getElementById("paint-area").getContext('2d');
@@ -20,20 +19,4 @@ $(".color-box").on("click", function(){
 	//change canvas border color
 	currentColor = newColor;
 	canvas.css({"border-color": currentColor});
-})
-=======
-//page fade animation
-$("body").animate({opacity:1}, 2000);
-
-//Grid pixel/class name animation
-$("#grid_system .row div").hover(function() {
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 0, left: "20px"}, 400);
-    that.find("h3").stop().animate({opacity: 1, right: "0px"}, 400);
-    
-}, function(){
-    var that = $(this);
-    that.find("h2").stop().animate({opacity: 1, left: "0px"}, 400);
-    that.find("h3").stop().animate({opacity: 0, right: "20px"}, 400);
 });
->>>>>>> FETCH_HEAD
