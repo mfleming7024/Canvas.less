1d3dd45eee6a741cfd58613573429905f9a377c8
Michael Fleming <mfleming7024@gmail.com>
2014-06-24 21:18:42 -0400

    v0.11
    - Added in color change on touch events for differentiation

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 var canvas = $("canvas");
-var currentColor = 000000;
+var currentColor = "#000000";
 var ctx = document.getElementById("paint-area").getContext('2d');
 
 canvas.on('touchstart', function(e) {
     e.preventDefault();
 	console.log("touch started");
+	currentColor = "#00ff00";
+	canvas.css({"border-color": currentColor});
 });
 
 canvas.on('touchmove', function(e) {
     e.preventDefault();
 	console.log("touch moved");
+	currentColor = "#ff0000";
+	canvas.css({"border-color": currentColor});
 });
 
 //Color changing
 var newColor;
 $(".color-box").on("click", function(){
 	newColor = $(this).attr("data-hex");
-	//change canvas border color
 	currentColor = newColor;
 	canvas.css({"border-color": currentColor});
 });
